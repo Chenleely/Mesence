91332cc04aa02ccf1ff716f76ab0e4f4bbcea537
chenkong <luozhihua@bilibili.com>
2023-08-03 19:24:50 +0800

    新增present方法

--- Mesence/util/MSViewControllerManager.swift
+++ Mesence/util/MSViewControllerManager.swift
@@ -44,4 +44,25 @@ class MSViewControllerManager {
             window.contentViewController = lastController
         }
     }
+    
+    func present(view: NSView?, size: CGSize) {
+        guard let window = self.window else  { return }
+        // create above sub window
+        window.backgroundColor = NSColor(hexString: "#CACACA")
+        let newWindowFrame = CGRect(x: window.frame.origin.x + window.frame.width / 2 - size.width / 2, y: window.frame.origin.y + window.frame.height / 2 - size.height / 2, width: size.width, height: size.height)
+        let newWindow = NSWindow(contentRect: newWindowFrame, styleMask: .borderless, backing: .buffered, defer: false)
+        newWindow.backgroundColor = .white
+        newWindow.level = .floating
+        newWindow.collectionBehavior = .transient
+        newWindow.contentView = view
+        window.addChildWindow(newWindow, ordered: .above)
+        
+        // bind them
+        let baseWindowFrame = window.frame
+        NotificationCenter.default.addObserver(forName: NSWindow.didMoveNotification, object: window, queue: nil) { (note) in
+            let newBaseWindowFrame = window.frame
+            let newNewWindowFrame = NSRect(x: newWindowFrame.origin.x + (newBaseWindowFrame.origin.x - baseWindowFrame.origin.x), y: newWindowFrame.origin.y + (newBaseWindowFrame.origin.y - baseWindowFrame.origin.y), width: newWindowFrame.size.width, height: newWindowFrame.size.height)
+            newWindow.setFrame(newNewWindowFrame, display: true)
+        }
+    }
 }
